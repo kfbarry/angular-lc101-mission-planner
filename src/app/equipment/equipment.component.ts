0af71c50d088aee34ceea44287a2fccfb6305df9
Kieran <kieranbarry51@gmail.com>
2022-03-22 23:42:51 -0500

    finished equipment bonus missions

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+itemBeingEdited: string = null;
+ equipment: string [] = ["Habitat dome","Drones","Food Containers","Oxygen Tanks"];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(item: string){
+    this.equipment.push(item);
+  }
+
+  edit(item: string){
+    this.itemBeingEdited = item;
+  }
+
+  save(updatedItem:string, item: string){
+    let index = this.equipment.indexOf(item)
+    this.equipment[index] = updatedItem; 
+    this.itemBeingEdited = null;
+  }
+
+  remove(item:string){
+    let index = this.equipment.indexOf(item)
+    this.equipment.splice(index,1);
+  }
 }
