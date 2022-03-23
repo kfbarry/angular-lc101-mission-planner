9d1e6835483c073ac8c852312dee867daa21beb6
Kieran <kieranbarry51@gmail.com>
2022-03-22 23:59:26 -0500

    completed studio and all bonus missions

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+ chosenExperiment: string = null;
+  experiments: string[] = ["Mars soil sample","Plant growth in habitat","Human bone density"];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(trial:string){
+    this.experiments.push(trial);
+  }
+
+  save(newTrial:string, trial:string){
+    let index = this.experiments.indexOf(trial);
+    this.experiments[index] = newTrial;
+    this.chosenExperiment = null;
+  }
+
+  edit(trial: string){
+    this.chosenExperiment = trial;
+  }
+
+  remove(trial:string){
+    let index = this.experiments.indexOf(trial);
+    this.experiments.splice(index,1)
   }
 
 }
