c149803c7014c320891be0a9635d26bef2664f7a
Kieran <kieranbarry51@gmail.com>
2022-03-24 00:36:41 -0500

    completed part 1

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -17,15 +17,67 @@ export class EquipmentComponent implements OnInit {
        {name: 'Satellite', mass: 1200},
        {name: 'R2 Unit', mass: 32}
    ];
+  
    cargoHold: object[] = [];
    cargoMass: number = 0;
    maximumAllowedMass: number = 2000;
    maxItems: number = 10;
+   nearMaxMass: string = "plain";
+   remainingSpace: number = 2000;
+
+   belowMaxMass: boolean []= [true,true,true,true,true,true,true,true,true]
+   belowMaxItems: boolean = true;
 
    constructor() { }
 
    ngOnInit() { }
 
-   // Code your addItem function here:
-   
+   addItem(equipment: object){
+    this.cargoHold.push(equipment);
+    this.cargoMass += equipment.mass;
+    this.checkMaxItems();
+    this.calculateMassRemaining()
+
+    for (let i=0; i < this.equipmentItems.length; i++){
+    this.checkMaxMass(this.equipmentItems[i]);
+    }
+
+    if (this.cargoMass + 200 >= this.maximumAllowedMass){
+      this.nearMaxMass = "nearMaxMass";
+    }
+   }
+
+   checkMaxMass(equipment: object){
+     let index = this.equipmentItems.indexOf(equipment);
+      if (equipment.mass + this.cargoMass > this.maximumAllowedMass){
+        this.belowMaxMass[index] = false;
+      } else {
+        this.belowMaxMass[index] = true;
+      }
+  }
+
+  checkMaxItems(){
+    if (this.cargoHold.length === this.maxItems){
+      this.belowMaxItems = false;
+    } else {
+      this.belowMaxItems = true;
+    }
+}
+
+calculateMassRemaining(){
+ this.remainingSpace = this.maximumAllowedMass - this.cargoMass;
+}
+
+emptyHold(){
+  this.cargoHold = [];
+  this.cargoMass = 0;
+  this.nearMaxMass = "plain";
+  this.checkMaxItems();
+  this.calculateMassRemaining()
+
+  for (let i=0; i < this.equipmentItems.length; i++){
+  this.checkMaxMass(this.equipmentItems[i]);
+  }
+}
+
 }
